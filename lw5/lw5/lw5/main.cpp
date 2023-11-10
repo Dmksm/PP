b83ef9c2f4af48fa8247ef5e8d25459b9bcab743
maksim-chernydev <maksim-chernydev@DESKTOP-U2UTPU1>
2023-11-10 15:04:01 +0300

    добавил мутех

--- lw5/lw5/lw5/main.cpp
+++ lw5/lw5/lw5/main.cpp
@@ -6,6 +6,8 @@
 
 CRITICAL_SECTION FileLockingCriticalSection;
 
+HANDLE ghMutex;
+
 int ReadFromFile() {
 	std::fstream myfile("balance.txt", std::ios_base::in);
 	int result;
@@ -13,6 +15,13 @@ int ReadFromFile() {
 	myfile.close();
 
 	return result;
+}
+
+void ClearFile()
+{
+	std::fstream myfile("balance.txt", std::ios_base::out);
+	myfile.clear();
+	myfile.close();
 }
 
 void WriteToFile(int data) {
@@ -63,30 +72,148 @@ DWORD WINAPI DoWithdraw(CONST LPVOID lpParameter)
 	ExitThread(0);
 }
 
-int _tmain(int argc, _TCHAR* argv[])
+DWORD WINAPI DoDepositWithMutex(CONST LPVOID lpParameter)
 {
-	HANDLE* handles = new HANDLE[49];
+	DWORD dwWaitResult;
 
-	InitializeCriticalSection(&FileLockingCriticalSection);
+	// Request ownership of mutex.
+	dwWaitResult = WaitForSingleObject(
+		ghMutex,    // handle to mutex
+		INFINITE);  // no time-out interval
 
-	WriteToFile(0);
+	switch (dwWaitResult)
+	{
+		// The thread got ownership of the mutex
+	case WAIT_OBJECT_0:
+		__try {
+			Deposit((int)lpParameter);
+		}
 
-	SetProcessAffinityMask(GetCurrentProcess(), 1);
+		__finally {
+			// Release ownership of the mutex object
+			if (!ReleaseMutex(ghMutex))
+			{
+				// Handle error.
+				std::cout << "Got a error!" << std::endl;
+			}
+		}
+		break;
+
+		// The thread got ownership of an abandoned mutex
+		// The database is in an indeterminate state
+	case WAIT_ABANDONED:
+		return FALSE;
+	}
+	ExitThread(0);
+	return TRUE;
+}
+
+DWORD WINAPI DoWithdrawWithMutex(CONST LPVOID lpParameter)
+{
+	DWORD dwWaitResult;
+
+	// Request ownership of mutex.
+
+	dwWaitResult = WaitForSingleObject(
+		ghMutex,    // handle to mutex
+		INFINITE);  // no time-out interval
+
+	switch (dwWaitResult)
+	{
+		// The thread got ownership of the mutex
+	case WAIT_OBJECT_0:
+		__try {
+			Withdraw((int)lpParameter);
+		}
+
+		__finally {
+			// Release ownership of the mutex object
+			if (!ReleaseMutex(ghMutex))
+			{
+				// Handle error.
+				std::cout << "Got a error!" << std::endl;
+			}
+		}
+		break;
+
+		// The thread got ownership of an abandoned mutex
+		// The database is in an indeterminate state
+	case WAIT_ABANDONED:
+		return FALSE;
+	}
+	ExitThread(0);
+	return TRUE;
+}
+
+void StartDepositAndWithdrawWithMutex(HANDLE* handles)
+{
+	for (int i = 0; i < 50; i++) {
+		handles[i] = (i % 2 == 0)
+			? CreateThread(NULL, 0, &DoDepositWithMutex, (LPVOID)230, CREATE_SUSPENDED, NULL)
+			: CreateThread(NULL, 0, &DoWithdrawWithMutex, (LPVOID)1000, CREATE_SUSPENDED, NULL);
+		ResumeThread(handles[i]);
+	}
+}
+
+void StartDepositAndWithdraw(HANDLE* handles)
+{
 	for (int i = 0; i < 50; i++) {
 		handles[i] = (i % 2 == 0)
 			? CreateThread(NULL, 0, &DoDeposit, (LPVOID)230, CREATE_SUSPENDED, NULL)
 			: CreateThread(NULL, 0, &DoWithdraw, (LPVOID)1000, CREATE_SUSPENDED, NULL);
 		ResumeThread(handles[i]);
 	}
+}
+
+void RunWithCriticalSection()
+{
+	HANDLE* handles = new HANDLE[50];
+
+	InitializeCriticalSection(&FileLockingCriticalSection);
+	WriteToFile(0);
+
+	SetProcessAffinityMask(GetCurrentProcess(), 1);
+	StartDepositAndWithdraw(handles);
 
 
-	// ожидание окончания работы двух потоков
+	// ожидание окончания работы 50 потоков
 	WaitForMultipleObjects(50, handles, true, INFINITE);
 	printf("Final Balance: %d\n", GetBalance());
 
 	getchar();
 
 	DeleteCriticalSection(&FileLockingCriticalSection);
+}
+
+void RunWithMutex()
+{
+	ClearFile();
+	HANDLE* handlesForMutex = new HANDLE[50];
+
+	ghMutex = CreateMutex(
+		NULL,              // default security attributes
+		FALSE,             // initially not owned
+		NULL);             // unnamed mutex
+
+	WriteToFile(0);
+
+	SetProcessAffinityMask(GetCurrentProcess(), 1);
+	StartDepositAndWithdrawWithMutex(handlesForMutex);
+
+	// ожидание окончания работы 50 потоков
+	WaitForMultipleObjects(50, handlesForMutex, true, INFINITE);
+	CloseHandle(ghMutex);
+	printf("Final Balance: %d\n", GetBalance());
+
+	getchar();
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	//std::cout << "-------------CriticalSection-------------" << std::endl;
+	//RunWithCriticalSection();
+	std::cout << "-------------With mutex-------------" << std::endl;
+	RunWithMutex();
 
 	return 0;
 }
